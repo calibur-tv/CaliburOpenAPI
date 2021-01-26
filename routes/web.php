78eef89cd6198fff78938e2c61c45f91e0fb4b2e
falstack <icesilt@outlook.com>
2021-01-26 09:26:08 +0800

    first: push

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+use Illuminate\Support\Facades\Redis;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router)
+{
+    $count = Redis::GET('count');
+    if (!$count)
+    {
+        $count = 1;
+    }
+    Redis::SET('count', $count + 1);
+
+    return $router->app->version() . '；visit count：' . $count . '；swoole：' . swoole_version();
+});
