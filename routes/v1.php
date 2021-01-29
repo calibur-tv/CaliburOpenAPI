5a75efb4e25c925d026a2e171d262de45fe47332
falstack <icesilt@outlook.com>
2021-01-29 11:08:51 +0800

    update: route

--- routes/v1.php
+++ routes/v1.php
@@ -2,14 +2,53 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-$router->get('/', function () use ($router)
+$router->group(['prefix' => 'sign'], function () use ($router)
 {
-    $count = Redis::GET('count');
-    if (!$count)
-    {
-        $count = 1;
-    }
-    Redis::SET('count', $count + 1);
+    $router->post('/detect', 'SignController@detectAccess');
 
-    return $router->app->version() . '；visit count：' . $count . '；swoole：' . swoole_version();
+    $router->post('/message', 'SignController@sendMessage');
+
+    $router->post('/register', 'SignController@register');
+
+    $router->post('/login', 'SignController@login');
+
+    $router->group(['middleware' => 'auth'], function () use ($router)
+    {
+        $router->post('/get_user_info', 'SignController@getUserInfo');
+
+        $router->post('/logout', 'SignController@logout');
+
+        $router->post('/oauth_channel', 'SignController@OauthChannelVerify');
+
+        $router->post('/bind_phone', 'SignController@bindPhone');
+
+        $router->post('/bind_weapp_user', 'SignController@bindWechatUser');
+
+        $router->post('/bind_qq_user', 'SignController@bindQQUser');
+    });
+
+    $router->post('/get_wechat_phone', 'SignController@getWechatPhone');
+
+    $router->post('/wechat_mini_app_login', 'SignController@wechatMiniAppLogin');
+
+    $router->post('/wechat_mini_app_get_token', 'SignController@wechatMiniAppToken');
+
+    $router->post('/weapp_mini_app_login', 'SignController@wechatMiniAppLogin');
+
+    $router->post('/weapp_mini_app_get_token', 'SignController@wechatMiniAppToken');
+
+    $router->post('/qq_mini_app_login', 'SignController@qqMiniAppLogin');
+
+    $router->post('/qq_mini_app_get_token', 'SignController@qqMiniAppToken');
+
+    $router->post('/reset_password', 'SignController@resetPassword');
+
+    $router->group(['prefix' => '/oauth2'], function () use ($router)
+    {
+        $router->get('/ticket', 'SignController@shareTicket');
+
+        $router->post('/qq', 'SignController@qqAuthRedirect');
+
+        $router->post('/wechat', 'SignController@wechatAuthRedirect');
+    });
 });
