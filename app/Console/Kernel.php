33bdfb16fc04002407e4cba868b362e467615a91
falstack <icesilt@outlook.com>
2021-04-09 09:19:52 +0800

    chore: stop cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('Test')->everyMinute();
-        $schedule->command('GetCharacter')->everyMinute();
+//        $schedule->command('Test')->everyMinute();
+//        $schedule->command('GetCharacter')->everyMinute();
     }
 }
